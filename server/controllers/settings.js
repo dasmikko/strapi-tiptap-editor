36be93dec375c851fba27800d951c9ea1925cf1f
dasmikko <dasmikko@gmail.com>
2022-03-17 09:25:01 +0100

    progress

--- server/controllers/settings.js
+++ server/controllers/settings.js
@@ -5,7 +5,16 @@ module.exports = {
     ctx.send(await strapi.store({type: 'plugin', name: 'strapi-tiptap-editor', key: 'settings'}).get())
   },
   async updateSettings(ctx) {
-    await strapi.store({type: 'plugin', name: 'strapi-tiptap-editor', key: 'settings'}).set({value: {settings: 'default value'}})
+    const defaultSettings = {
+      headings: ['h1', 'h2', 'h3', 'h4', 'h4', 'h5', 'h6'],
+      bold: true,
+      italic: true,
+      strikethrough: true,
+      underline: true,
+      align: ['left', 'center', 'right']
+    }
+
+    await strapi.store({type: 'plugin', name: 'strapi-tiptap-editor', key: 'settings'}).set({value: defaultSettings})
     ctx.send({res: 'ok'})
   },
 
